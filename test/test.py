e8a8db2330ead1f4cb8c99db5d36efd8fe7ba045
Shaiful Alam <saifcce@gmail.com>
2017-04-13 16:12:00 +0600

    Update test.py

--- test/test.py
+++ test/test.py
@@ -1,8 +1,8 @@
-print("Hello, World!")
-print("Hello, World Again!")
-print("Hello, World!")
-print("Hello, World Again!")
-print("Hello, World!")
-print("Hello, World Again!")
-print("Hello, World!")
-print("Hello, World Again!")
+#!/usr/bin/python
+
+count = 0
+while (count < 999999999):
+   print 'The count is:', count
+   count = count + 1
+
+print "Good bye!"
